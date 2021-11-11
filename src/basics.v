a16de77237de1b0c2d5850ac7074b8ccf4482fca
Mark Martynov <mark.martynov@juspay.in>
2021-11-12 02:33:33 +0300

    Review fixes for basics

--- src/basics.v
+++ src/basics.v
@@ -9,26 +9,12 @@ Implicit Types (xs ys zs: seq T).
 
 (* 1.4 Proofs *)
 
-Lemma list_app xs ys zs : (xs ++ ys) ++ zs = xs ++ ys ++ zs.
-Proof.
-elim: xs => [| a xs IH] //.
-by rewrite !cat_cons IH.
-Qed.
-
 (* TODO: 1.4.1 gcd example *)
 
 (* 1.5 Running time *)
 
-Print app.
-
 Fixpoint T_app xs ys : nat :=
-  match xs with
-  | [::] => 1
-  | _ :: xs' => (T_app xs' ys).+1
-  end.
-
-Fixpoint T_app' xs ys : nat :=
-  if xs is _ :: xs' then (T_app' xs' ys).+1 else 1.
+  if xs is _ :: xs' then (T_app xs' ys).+1 else 1.
 
 (* Not a real rev implementation from lib *)
 Fixpoint rev' xs :=
@@ -40,7 +26,7 @@ Fixpoint T_rev xs : nat :=
 Lemma T_app_cons x xs ys : T_app (x :: xs) ys = (T_app xs ys).+1.
 Proof. by []. Qed.
 
-Lemma T_app_complexity xs ys : T_app xs ys = (length xs).+1.
+Lemma T_app_complexity xs ys : T_app xs ys = (size xs).+1.
 Proof.
 elim: xs => [| x xs IH] //.
 by rewrite T_app_cons IH.
@@ -48,15 +34,12 @@ Qed.
 
 (* Exercise 1.5.1 formula for T_rev *)
 
+Fixpoint T_catrev xs ys : nat :=
+  if xs is x :: xs' then (T_catrev xs' (x :: ys)).+1 else 1.
 
-Definition iterv {T} := @catrev T.
-
-Fixpoint T_iterv xs ys : nat :=
-  if xs is x :: xs' then (T_iterv xs' (x :: ys)).+1 else 1.
-
-Lemma iterv_rev_equality xs ys : iterv xs ys = rev' xs ++ ys.
+Lemma catrev_rev_equality xs ys : catrev xs ys = rev' xs ++ ys.
 Proof.
-rewrite /iterv.
+rewrite /catrev.
 elim: xs ys => [| x xs IH] ys //=.
 by rewrite IH -catA.
 Qed.
